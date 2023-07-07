a25a79fb6c72e439f11813287f05f44a174d63e6
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-07-07 20:01:35 +0200

    mejorando Footer

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -28,13 +28,27 @@ import modeTheme from "~/components/modeTheme.vue";
       <div
         class="sm:relative flex flex-col items-center gap-3 sm:flex-row sm:justify-between justify-center"
       >
-      <modeTheme />
+        <modeTheme />
         <div
           class="flex flex-col-reverse items-center gap-3 sm:flex-row sm:w-40 md:w-60"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            class="icon icon-leading"
+            style="width: 1.5em; height: 1.5em"
+            viewBox="0 0 512 512"
           >
+            <path
+              fill="currentColor"
+              d="M508.811 228.84C503.111 156.84 444.811 98.54 372.811 98.54c-8.131 0-16.099.88-23.94 2.54l-71.06 141.08l-68.66-29.16c-9.57-4.07-20.86-1.41-28.15 6.82L83.66 265.59c-9.84 9.84-11.82 24.81-5.41 36.95L187.41 469c4.61 8.21 14.2 11.31 22.62 7.95l110.7-47.79l137.36 68.68c12.15 6.41 27.11 4.43 36.95-5.41l91.72-91.72c8.21-8.21 11.89-20.15 7.97-31.36zm-112.42-33.36l-113.86 113.86L164.27 237.48l113.86-113.86l128.29 128.29zm-213.16 64.44c0 42.49-34.53 77.02-77.02 77.02S29.61 302.77 29.61 260.28c0-42.49 34.53-77.02 77.02-77.02s77.02 34.53 77.02 77.02z"
+            ></path>
+          </svg>
           <span class="text-sm u-text-gray-500"
-          >Spain Madrid Villanueva nº5, 1º 28001
-        </span>
+            >Spain Madrid Villanueva nº5, 1º 28001
+          </span>
         </div>
         <ul class="flex flex-wrap justify-center text-sm gap-x-6 gap-y-2">
           <!--[-->
@@ -50,7 +64,7 @@ import modeTheme from "~/components/modeTheme.vue";
         <ul class="flex items-center justify-end gap-3 z-10 sm:w-40 md:w-60">
           <li>
             <a
-              href="https://twitter.com/nuxt_js"
+              href="https://www.linkedin.com/company/sea-&-ports-mgm/mycompany/"
               rel="noopener noreferrer"
               target="_blank"
               class="app-button pv-pVi5oy pc-DUJjyq !p-0 u-text-gray-900 focus-visible:ring-2"
@@ -62,57 +76,62 @@ import modeTheme from "~/components/modeTheme.vue";
                 aria-hidden="true"
                 role="img"
                 class="icon icon-leading"
-                data-v-fae88b39=""
-                style=""
-                width="1em"
-                height="1em"
-                viewBox="0 0 24 24"
-                data-v-9aa0cd35=""
+                style="width: 1em; height: 1em"
+                viewBox="0 0 448 512"
               >
                 <path
                   fill="currentColor"
-                  d="M22 5.8a8.49 8.49 0 0 1-2.36.64a4.13 4.13 0 0 0 1.81-2.27a8.21 8.21 0 0 1-2.61 1a4.1 4.1 0 0 0-7 3.74a11.64 11.64 0 0 1-8.45-4.29a4.16 4.16 0 0 0-.55 2.07a4.09 4.09 0 0 0 1.82 3.41a4.05 4.05 0 0 1-1.86-.51v.05a4.1 4.1 0 0 0 3.3 4a3.93 3.93 0 0 1-1.1.17a4.9 4.9 0 0 1-.77-.07a4.11 4.11 0 0 0 3.83 2.84A8.22 8.22 0 0 1 3 18.34a7.93 7.93 0 0 1-1-.06a11.57 11.57 0 0 0 6.29 1.85A11.59 11.59 0 0 0 20 8.45v-.53a8.43 8.43 0 0 0 2-2.12Z"
+                  d="M416 32H31.9C14.3 32 0 46.3 0 63.9v384.2C0 465.7 14.3 480 31.9 480H416c17.6 0 32-14.3 32-32V63.9c0-17.6-14.3-31.9-32-31.9zM135.5 416H69V214.9h66.5V416zm-33.2-242.4c-21.3 0-38.6-17.3-38.6-38.6c0-21.3 17.3-38.6 38.6-38.6c21.3 0 38.6 17.3 38.6 38.6c0 21.3-17.3 38.6-38.6 38.6zm280.7 242.4h-66.5V316c0-22.4-.4-51.1-31.2-51.1c-31.2 0-35.9 24.4-35.9 49.5v101.9h-66.4V214.9h63.6v26.3h.9c8.9-16.8 30.6-34.5 63.1-34.5c67.5 0 79.9 44.5 79.9 102.3V416z"
                 ></path></svg
               ><span data-v-fae88b39=""
-                ><!--[--><span class="sr-only">Follow Nuxt on Twitter</span
+                ><!--[--><span class="sr-only"
+                  >Follow Sea & Ports on Linkedin</span
                 ><!--]--></span
               ><!----></a
             >
           </li>
           <li>
             <a
-              href="https://discord.com/invite/ps2h6QT"
+              href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJqVwhkNVpcRKkpQscPtGWJmHfJZQHsvgnfwKCtfkdDgjQcSxdhQnffhgWjHQQgdmMHRbhg"
               rel="noopener noreferrer"
               target="_blank"
               class="app-button pv-pVi5oy pc-ZEVo7J !p-0 u-text-gray-900 focus-visible:ring-2"
               title="marguisa@marguisa.com"
               data-v-fae88b39=""
-              ><svg
+            >
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                aria-hidden="true"
-                role="img"
-                class="icon icon-leading"
-                data-v-fae88b39=""
-                style=""
-                width="1em"
-                height="1em"
-                viewBox="0 0 640 512"
-                data-v-9aa0cd35=""
+                viewBox="0 0 512 640"
+                style="height: 1.5em"
               >
                 <path
                   fill="currentColor"
-                  d="M524.531 69.836a1.5 1.5 0 0 0-.764-.7A485.065 485.065 0 0 0 404.081 32.03a1.816 1.816 0 0 0-1.923.91a337.461 337.461 0 0 0-14.9 30.6a447.848 447.848 0 0 0-134.426 0a309.541 309.541 0 0 0-15.135-30.6a1.89 1.89 0 0 0-1.924-.91a483.689 483.689 0 0 0-119.688 37.107a1.712 1.712 0 0 0-.788.676C39.068 183.651 18.186 294.69 28.43 404.354a2.016 2.016 0 0 0 .765 1.375a487.666 487.666 0 0 0 146.825 74.189a1.9 1.9 0 0 0 2.063-.676A348.2 348.2 0 0 0 208.12 430.4a1.86 1.86 0 0 0-1.019-2.588a321.173 321.173 0 0 1-45.868-21.853a1.885 1.885 0 0 1-.185-3.126a251.047 251.047 0 0 0 9.109-7.137a1.819 1.819 0 0 1 1.9-.256c96.229 43.917 200.41 43.917 295.5 0a1.812 1.812 0 0 1 1.924.233a234.533 234.533 0 0 0 9.132 7.16a1.884 1.884 0 0 1-.162 3.126a301.407 301.407 0 0 1-45.89 21.83a1.875 1.875 0 0 0-1 2.611a391.055 391.055 0 0 0 30.014 48.815a1.864 1.864 0 0 0 2.063.7A486.048 486.048 0 0 0 610.7 405.729a1.882 1.882 0 0 0 .765-1.352c12.264-126.783-20.532-236.912-86.934-334.541ZM222.491 337.58c-28.972 0-52.844-26.587-52.844-59.239s23.409-59.241 52.844-59.241c29.665 0 53.306 26.82 52.843 59.239c0 32.654-23.41 59.241-52.843 59.241Zm195.38 0c-28.971 0-52.843-26.587-52.843-59.239s23.409-59.241 52.843-59.241c29.667 0 53.307 26.82 52.844 59.239c0 32.654-23.177 59.241-52.844 59.241Z"
+                  d="M480 192c-1.1 0-2.2 0-3.3.1L256 272L35.3 192H480zm0-32H32l224 80l224-80zM73.2 260.5l-4.9 15.5l47 37.6L73.2 260.5zm363.6 0l-47.1 37.7l47 37.6l-4.9-15.5zM16 269.4V400c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16V269.4L256 349L16 269.4z"
                 ></path></svg
               ><span data-v-fae88b39=""
-                ><!--[--><span class="sr-only">marguisa@marguisa.com</span
+                ><!--[--><span class="sr-only">Write Marguisa</span
                 ><!--]--></span
               ><!----></a
             >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-mail"
+            >
+              <path
+                d="M20 4H4C2.89543 4 2 4.89543 2 6V18C2 19.1046 2.89543 20 4 20H20C21.1046 20 22 19.1046 22 18V6C22 4.89543 21.1046 4 20 4Z"
+              ></path>
+              <path d="M22 6L12 12L2 6"></path>
+            </svg>
           </li>
           <li>
             <a
-              href="https://github.com/nuxt/nuxt"
+              href="https://github.com/lamjo30?tab=followers"
               rel="noopener noreferrer"
               target="_blank"
               class="app-button pv-pVi5oy pc-OWNT7- !p-0 u-text-gray-900 focus-visible:ring-2"
