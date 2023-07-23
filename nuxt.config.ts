cfc842fb878e7b234ba3c96e23ec3317ae526514
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-07-23 18:45:51 +0200

    Agregando Three

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,7 @@ export default defineNuxtConfig({
         // <meta name="viewport" content="width=device-width, initial-scale=1">
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
-      script: [
-        // <script src="https://myawesome-lib.js"></script>
-      ],
+      script: [ {src: "https://unpkg.com/@splinetool/viewer@0.9.394/build/spline-viewer.js"}],
       // please note that this is an area that is likely to change
       style: [
         // <style type="text/css">:root { color: red }</style>
@@ -20,12 +18,13 @@ export default defineNuxtConfig({
       ],
     },
   },
+  
   // devtools: { enabled: true },
-  modules: ["@nuxtjs/i18n", '@nuxtjs/color-mode', '@element-plus/nuxt'],
+  modules: ["@nuxtjs/i18n", "@nuxtjs/color-mode", "@element-plus/nuxt"],
   i18n: {
-    locales: ['en', 'fr', 'es'],
-    defaultLocale: 'es',
-    strategy: 'prefix_and_default'
+    locales: ["en", "fr", "es"],
+    defaultLocale: "es",
+    strategy: "prefix_and_default",
   },
   css: ["~/assets/css/main.css"],
   postcss: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
   <section class="index__pages-video h-full w-full">
-    <video src="../assets/img/video-home-f2.mp4" autoplay muted></video>
-    <p>{{ $t("welcome") }}</p>
+    <div class="container mx-auto">
+      <div class="grid grid-rows-2 grid-flow-none md:grid-flow-col gap-4">
+        <div
+        class="row-span-2 col-span-1 w-full h-64 bg-[#06f] rounded-lg"
+        ></div>
+        <div class="row-span-3">
+          <div class="flex flex-wrap">
+            <div class="basis-1/4">PP</div>
+            <div class="basis-1/2">PP</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
